174eb99d34a1734b2dd296d971ea4b53c112c487
Khaled Hesham <khaledhesham007@gmail.com>
2024-06-11 05:51:15 +0300

    v0.1.a

---
+++ Rock Paper Scissors/main.py
@@ -0,0 +1,29 @@
+import random
+
+winning_combinations = {
+    'R': 'S',  # Rock beats Scissors
+    'P': 'R',  # Paper beats Rock
+    'S': 'P'   # Scissors beats Paper
+}
+choices = ['R', 'P', 'S']
+player = None
+
+while player != '0':
+    
+    player = input("Your Move.. \n(R -> Rock, P -> Paper, S -> Scissors)\nExit -> 0\n").upper()
+    print(f"Player Move: {player}")
+
+    pc = random.choice(choices)
+    print(f"PC Move: {pc}")
+
+    if player not in choices:
+        print("Invalid input, please try again.")
+        continue
+
+    if player == pc:
+        print("TIE!!")
+    elif pc == winning_combinations.get(player):
+        print("PLAYER WINS!!")
+    else:
+        print("PC WINS!!")
+        
