ab42da0cbee2a55a1e2a58728dbbbad2f024bfd6
Khaled Hesham <khaledhesham007@gmail.com>
2024-06-26 16:33:55 +0300

    ScraperV1

--- Web Scrapper/main.py
+++ Web Scrapper/main.py
@@ -1,1 +1,13 @@
-print("test test")
+import requests
+from bs4 import BeautifulSoup
+# website = input("Enter Website: ")
+
+response = requests.get("https://books.toscrape.com/")
+
+#soup = input("Enter soup: ")
+
+soup = BeautifulSoup(response.content, "html.parser")
+
+books = soup.find_all("article")
+
+print(books)
